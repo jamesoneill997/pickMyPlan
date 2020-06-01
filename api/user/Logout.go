7e76e9ee159fc118b53c6c57070a22ebd662f674
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-01 20:10:53 +0100

    refactored user.Logout

--- api/user/Logout.go
+++ api/user/Logout.go
@@ -5,11 +5,22 @@ import (
 	"time"
 )
 
+//Logout function instantly expires token
 func Logout(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
+	//Handle POST method
 	case "POST":
+		//generate cookie and set
 		cookie := http.Cookie{Name: "Token", Expires: time.Now()}
-
 		http.SetCookie(w, &cookie)
+		w.WriteHeader(200)
+		w.Write([]byte("Successfully logged out"))
+
+	//Ignore any other request method
+	default:
+		w.WriteHeader(400)
+		w.Write([]byte("Bad Request"))
+
 	}
+
 }
