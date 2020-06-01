285225440cc0f9ded7ed59e14b1a1408fa083af8
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-01 20:05:07 +0100

    refactored user.CreateUser and db.AddUser. Response codes fully functional

--- api/user/CreateUser.go
+++ api/user/CreateUser.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"golang.org/x/crypto/bcrypt"
@@ -11,32 +10,71 @@ import (
 	template "github.com/jamesoneill997/pickMyPlan/templates"
 )
 
+//CreateUser > add to db > generate auth token > set cookie
 func CreateUser(w http.ResponseWriter, r *http.Request) {
+	//connect to database
 	client := db.SetConnection()
 	userCol := db.ConnectCollection(client, "users")
+
+	//handle requests, POST is to be handled, all else should be rejected
 	switch r.Method {
 	case http.MethodPost:
+
+		//read submitted data and store in user struct
 		decoder := json.NewDecoder(r.Body)
 		u := template.User{}
 		decodeErr := decoder.Decode(&u)
-		password := []byte(u.Password)
 
-		hashedPassword, passErr := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-
-		if decodeErr != nil || passErr != nil {
+		//handle err
+		if decodeErr != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("Invalid data submitted"))
 			return
 		}
-		u.Password = string(hashedPassword)
-		db.AddUser(*userCol, u)
-		userToken, err := GenerateToken()
-		if err != nil {
-			fmt.Println("Error generating token: ", err)
+
+		//encrypt password
+		password := []byte(u.Password)
+		hashedPassword, passErr := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+
+		//handle err
+		if passErr != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Internal Server Error"))
+			return
 		}
+
+		//set user password to encrypted password before storage
+		u.Password = string(hashedPassword)
+
+		//add user to db
+		create := db.AddUser(*userCol, u)
+
+		//handle err
+		if create != 0 {
+			w.WriteHeader(500)
+			w.Write([]byte("Error creating account. Please try again."))
+			return
+		}
+
+		//generate auth token for user
+		userToken, tokeErr := GenerateToken()
+		if tokeErr != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Internal Server Error"))
+			return
+		}
+
+		//create and set cookie (no expiry)
 		c := http.Cookie{Name: "Token", Value: userToken}
 
 		http.SetCookie(w, &c)
+		//creation response
+		w.WriteHeader(201)
+		w.Write([]byte("User created successfully"))
 		return
 	default:
+		//Any request other than POST will be ignored
+		w.WriteHeader(400)
 		return
 	}
 }
--- db/AddUser.go
+++ db/AddUser.go
@@ -8,15 +8,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+//AddUser function adds a user to the DB, returns non-zero exit status for error
 func AddUser(userCol mongo.Collection, user template.User) int {
 	//add user
 	res, err := userCol.InsertOne(context.Background(), user)
+
+	//handle err
 	if err != nil {
 		fmt.Println(err)
 		return -1
-	} else {
-		fmt.Println("User added successfully")
-		fmt.Println(res)
-		return 0
 	}
+
+	//success, print object id
+	fmt.Println(res.InsertedID)
+	return 0
+
 }
