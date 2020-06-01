ba4fd5a075dfb555f7ff94efe38a206aafc5ba4d
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-01 22:04:49 +0100

    refactored db.ConnectCollection

--- db/ConnectCollection.go
+++ db/ConnectCollection.go
@@ -2,8 +2,11 @@ package db
 
 import "go.mongodb.org/mongo-driver/mongo"
 
+//ConnectCollection will connect to Mongodb collection that is specified
 func ConnectCollection(client *mongo.Client, collName string) *mongo.Collection {
-	//connect to users db
+	//connect to db and specified collection
 	collection := client.Database("pickMyPlan").Collection(collName)
+
+	//return pointer to mongo.Collection type
 	return collection
 }
