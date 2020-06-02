1db0ef8181c6e01eb7346b681b4a8fd865caaaab
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-02 11:11:06 +0100

    refactored user.UpdateUser

--- api/user/UpdateUser.go
+++ api/user/UpdateUser.go
@@ -8,10 +8,35 @@ import (
 
 //UpdateDetails func
 func UpdateDetails(w http.ResponseWriter, r *http.Request) {
-	query := GetAllQueries(w, r)
-	field := query["field"][0]
-	username := query["username"][0]
-	newValue := query["value"][0]
 
-	db.UpdateDetails(username, field, newValue)
+	switch r.Method {
+
+	//handle PATCH requests
+	case "PATCH":
+		//parse request
+		query := GetAllQueries(w, r)
+		field := query["field"][0]
+		username := query["username"][0]
+		newValue := query["value"][0]
+
+		//update on the database
+		result, err := db.UpdateDetails(username, field, newValue)
+
+		//error with the database update
+		if result != 0 || err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Internal server error"))
+			return
+		}
+
+		//Success, 200 response
+		w.WriteHeader(200)
+		w.Write([]byte("Update successfully completed"))
+
+	//ignore all other request methods
+	default:
+		w.WriteHeader(403)
+		w.Write([]byte("Not authorized"))
+		return
+	}
 }
