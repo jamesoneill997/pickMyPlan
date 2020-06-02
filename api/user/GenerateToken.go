dda9363420cecd1b2553f450afe14a039d4892df
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-02 10:26:03 +0100

    refactored user.IsAuthorised middleware

--- api/user/GenerateToken.go
+++ api/user/GenerateToken.go
@@ -9,21 +9,26 @@ import (
 //needs to be env var in production environment
 var signingKey = []byte("Myawesomesigningkeyisthisstring")
 
+//GenerateToken function will generate a jwt for the user. Returns token or err
 func GenerateToken() (string, error) {
+	//setup signing method
 	token := jwt.New(jwt.SigningMethodHS256)
 
+	//setup claims
 	claims := token.Claims.(jwt.MapClaims)
-
 	claims["authorized"] = true
 	claims["user"] = "James O'Neill"
 	claims["exp"] = 2147483647
 
+	//Sign with signing key
 	tokenString, err := token.SignedString(signingKey)
 
+	//handle err
 	if err != nil {
 		fmt.Println(err)
 		return "", err
 	}
 
+	//success, return token string
 	return tokenString, nil
 }
--- api/user/IsAuthorized.go
+++ api/user/IsAuthorized.go
@@ -1,36 +1,64 @@
 package user
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
+//needs to be env var in production environment
 var mySigningKey = []byte("Myawesomesigningkeyisthisstring")
 
+//IsAuthorized - Middleware function that checks if a user has acceess to an endpoint
 func IsAuthorized(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	//return http.Handler type
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["Cookie"] != nil {
-			cookieBody := r.Header.Get("Cookie")[6:]
-			token, err := jwt.Parse(cookieBody, func(token *jwt.Token) (interface{}, error) {
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					return nil, fmt.Errorf("There was an error")
+		switch r.Method {
+		//handle GET requests
+		case "GET":
+			//check if cookie exists
+			if r.Header["Cookie"] != nil {
+				//get cookie from request header, 6: takes out "Token=" string which causes int64 error
+				cookieBody := r.Header.Get("Cookie")[6:]
+				//parse cookie body and check token, 2nd param is a function which returns the signingKey
+				token, parseErr := jwt.Parse(cookieBody, func(token *jwt.Token) (interface{}, error) {
+					//handle err
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, errors.New("Unauthorized")
+					}
+
+					//return signing key to parse
+					return mySigningKey, nil
+				})
+
+				//handle err
+				if parseErr != nil || !token.Valid {
+					w.WriteHeader(403)
+					w.Write([]byte("Unauthorized"))
+					fmt.Println(parseErr)
+					fmt.Printf("%T ----- %v", token, *token)
+					return
 				}
 
-				return mySigningKey, nil
-			})
-
-			if err != nil {
-				fmt.Println("Error with signature", err, "\n", token)
-			}
-
-			if token.Valid {
+				//Authorized, 200 response and serve endpoint
+				w.WriteHeader(200)
+				w.Write([]byte("Access granted"))
 				endpoint(w, r)
-			}
+				return
 
-		} else {
-			fmt.Println("Cookie is nil")
+			}
+			//if no cookie, user does not have access
+			w.WriteHeader(403)
+			w.Write([]byte("Unauthorized"))
+			return
+
+		//ignore all other request methods
+		default:
+			w.WriteHeader(401)
+			w.Write([]byte("Bad request"))
+			return
 		}
 	})
 }
