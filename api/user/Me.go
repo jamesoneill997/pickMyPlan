6eab19c6ea40ae6da759f3e294674f4b104ea83d
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-03 16:34:38 +0100

    refactored user.GetMe and user.GetUsername

--- api/user/Me.go
+++ api/user/Me.go
@@ -3,21 +3,27 @@ package user
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/dgrijalva/jwt-go"
 )
-
-var t jwt.MapClaims
 
 //GetMe will return user information which is only accessible to the current user
 func GetMe(w http.ResponseWriter, r *http.Request) {
+
+	//Parse value from key, value pair
 	authToken := r.Header["Cookie"][0][6:]
+
+	//Parse username from auth token
 	username, err := GetUsername(authToken)
+
+	//handle err
 	if err != nil {
 		w.WriteHeader(503)
 		w.Write([]byte("Internal server error"))
+		return
 	}
 
+	//Success, 200 response
+	w.WriteHeader(200)
+	w.Write([]byte("Profile fetched successfully"))
 	fmt.Println("Your username is: ", username)
 
 }
--- api/user/TokenParse.go
+++ api/user/TokenParse.go
@@ -5,26 +5,37 @@ import (
 
 	"github.com/dgrijalva/jwt-go"
 )
+var t jwt.MapClaims
 
+//GetUsername function to parse username from object. Used to determine current user
 func GetUsername(authToken string) (string, error) {
+	
+	//Parse token
 	token, err := jwt.ParseWithClaims(authToken, &t, func(token *jwt.Token) (interface{}, error) {
+		//call env var in production as opposed to signing key
 		return []byte("Myawesomesigningkeyisthisstring"), nil
 	})
 
+	//handle err
 	if err != nil {
 		fmt.Println(err)
 		return "Error", err
 	}
+
+	//Parse claims from token
 	claims := token.Claims.(*jwt.MapClaims)
 
+	//Cast claims to map for easy manipulation
 	claimMap := make(map[string]interface{})
 
+	//range over claims to get all
 	for k, v := range *claims {
 		claimMap[k] = v
 	}
 
+	//take user claim
 	username := claimMap["user"].(string)
-	return string(username), nil
 
-	
+	//Success, return username
+	return username, nil
 }
