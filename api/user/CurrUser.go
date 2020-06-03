e73846e35ad8a9567f5289b20da6685ac2f70260
jamesoneill997 <jamesoneill997@gmail.com>
2020-06-03 21:09:26 +0100

    refactored GetMe function and added CurrUser function

---
+++ api/user/CurrUser.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"net/http"
+
+	db "github.com/jamesoneill997/pickMyPlan/db"
+	template "github.com/jamesoneill997/pickMyPlan/templates"
+)
+
+//CurrUser function is used to return current user of type template.User
+func CurrUser(w http.ResponseWriter, r *http.Request) template.User {
+
+	//Parse value from key, value pair
+	authToken := r.Header["Cookie"][0][6:]
+
+	//Parse username from auth token
+	username, err := GetUsername(authToken)
+
+	//handle err
+	if err != nil {
+		w.WriteHeader(503)
+		w.Write([]byte("Internal server error"))
+		return template.User{}
+	}
+
+	user, err := db.FindUserByUsername(username)
+
+	if err != nil {
+		w.WriteHeader(503)
+		w.Write([]byte("Internal server error"))
+		return template.User{}
+	}
+
+	//Success, 200 response
+
+	w.Write([]byte("Profile fetched successfully\n"))
+	return user
+}
--- api/user/Me.go
+++ api/user/Me.go
@@ -1,39 +1,12 @@
 package user
 
 import (
-	"fmt"
 	"net/http"
-
-	db "github.com/jamesoneill997/pickMyPlan/db"
 )
 
 //GetMe will return user information which is only accessible to the current user
 func GetMe(w http.ResponseWriter, r *http.Request) {
+	me := CurrUser(w, r)
 
-	//Parse value from key, value pair
-	authToken := r.Header["Cookie"][0][6:]
-
-	//Parse username from auth token
-	username, err := GetUsername(authToken)
-
-	//handle err
-	if err != nil {
-		w.WriteHeader(503)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-
-	user, err := db.FindUserByUsername(username)
-
-	if err != nil {
-		w.WriteHeader(503)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-
-	//Success, 200 response
-	w.WriteHeader(200)
-	w.Write([]byte("Profile fetched successfully"))
-	fmt.Println(user)
-
+	w.Write([]byte(me.Username))
 }
